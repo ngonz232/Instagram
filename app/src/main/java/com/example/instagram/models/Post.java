1aaeface91969574db478c03670739877d8b7417
ngonz232 <ngonz232@fiu.edu>
2021-07-09 14:58:07 -0400

    testing git

--- app/src/main/java/com/example/instagram/models/Post.java
+++ app/src/main/java/com/example/instagram/models/Post.java
@@ -13,7 +13,7 @@ public class Post extends ParseObject {
     public static final String KEY_IMAGE = "image";
     public static final String KEY_NUM_LIKES = "numLikes";
     public static final String KEY_NUM_COMMENTS = "numComments";
-
+    //testing git
     public Post() {}
 
     public Post(ParseUser user, String description, ParseFile image) {
